cb619aa7200c2f8c733c6403c0a76bba5f10b6a9
Francesc Esplugas <francesc@intraducibles.com>
2010-11-05 15:31:58 +0100

    First commit ...

---
+++ README.md
@@ -0,0 +1,88 @@
+# Translate
+
+The main idea of this plugin is to keep all field translations on the 
+same table. I've worked with other Rails plugins but all of them have 
+some caveats, and I needed something really simple.
+
+With the release of Rails 2.2.X, with the I18n integrated now it's 
+easier to translate an Rails application, GetText is no longer needed. 
+So I've added a few tasks which allow to extract translatable text 
+from the models, views and controllers.
+
+    $ rake -T i18n
+
+## Quick explanation
+
+We have a page model with the following attributes.
+
+    Page: title_en, title_ca, title_es
+
+By default Rails defines the current locale as <tt>:en</tt> and the default 
+locale to <tt>:en</tt>. If we add <tt>translate :title</tt> to our model we can get 
+the content of the attribute without defining the locale, and we'll 
+get the localized field.
+
+    @page_title = Page.find(:first).title
+
+We can change the locale with <tt>I18n.locale</tt>.
+
+    I18n.locale = :ca
+    @page_title = Page.find(:first).title
+
+And we'll get the <tt>title</tt> attribute of the defined locale.
+
+Sample migration
+
+    class CreatePages < ActiveRecord::Migration
+
+      def self.up
+        create_table :pages do |t|
+          t.string :title_en, :null => false
+          t.string :title_es
+          t.string :title_ca
+          t.text :content_en, :null => false
+          t.text :content_es
+          t.text :content_ca
+          t.boolean :status, :null => false, :default => false
+        end
+      end
+
+      def self.down
+        drop_table :pages
+      end
+
+    end
+
+Model definition
+
+    # app/models/page.rg
+    class Page < ActiveRecord::Base
+      translate :title, :body
+    end
+
+## Usage from the console
+
+    $ script/console
+    Loading development environment (Rails 2.2.2)
+    >> I18n.default_locale
+    => :en
+    >> I18n.locale
+    => :en
+    >> Page.first.title
+    => "Hello World"
+    >> I18n.locale = :es
+    => :es
+    >> Page.first.title
+    => "Hola Mundo"
+    >> Page.first.update_attributes :title => "Hola Mundo"
+    => true
+    >> Page.first.title
+    => "Hola Mundo"
+    >> I18n.locale = :undefined
+    => :undefined
+    >> Page.first.title
+    => "Hello World"    <== Because is the default locale
+    >> Page.find_by_name("Hello World")
+    => ...
+
+Copyright (c) 2008-2010 Francesc Esplugas Marti, released under the MIT license
---
+++ test/translate_test.rb
@@ -0,0 +1,128 @@
+require 'test/unit'
+require 'rubygems'
+require 'active_record'
+
+$LOAD_PATH << File.expand_path("#{File.dirname(__FILE__)}/../lib")
+require 'translate'
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+
+  silence_stream(STDOUT) do
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table :pages, :force => true do |t|
+        t.string :name_en, :default => "", :null => false
+        t.string :name_es, :default => "", :null => false
+        t.text :body_en, :default => "", :null => false
+        t.text :body_es, :default => "", :null => false
+        t.datetime :created_at
+      end
+    end
+  end
+
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
+  end
+end
+
+class Page < ActiveRecord::Base
+  translate :name, 'body'
+end
+
+class TranslateTest < Test::Unit::TestCase
+
+  def setup
+
+    setup_db
+
+    Page.create :name_en => "Name", 
+                :name_es => "Nombre", 
+                :body_en => "Content", 
+                :body_es => "Contenido",
+                :created_at => Time.now - 60
+
+    Page.create :name_en => "Second Name",
+                :name_es => "Nombre",
+                :body_en => "Second Content",
+                :body_es => "Segundo Contenido",
+                :created_at => Time.now
+
+    I18n.default_locale = :en
+    I18n.locale = :en
+
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_should_get_title_en_of_the_page
+    page = Page.find(:first)
+    assert_equal page.send('name_en'), page.name
+  end
+
+  def test_should_get_title_es_of_the_page
+    I18n.locale = :es
+    page = Page.find(:first)
+    assert_equal page.send('name_es'), page.name
+  end
+
+  def test_should_return_title_en_when_undefined_locale
+    I18n.locale = :undefined
+    page = Page.find(:first)
+    assert_equal page.send('name_en'), page.name
+  end
+
+  def test_should_return_title_es_because_is_the_default_locale
+    I18n.default_locale = :es
+    I18n.locale = :undefined
+    page = Page.find(:first)
+    assert_equal page.send('name_es'), page.name
+  end
+
+  def test_should_find_by_translated_attribute
+
+    page = Page.find_by_name('Name')
+    assert_equal page.name, 'Name'
+    assert_equal page.body, 'Content'
+
+    I18n.locale = :es
+    page = Page.find_by_name('Nombre')
+    assert_equal page.name, 'Nombre'
+
+  end
+
+  def test_should_find_with_options_with_default_locale
+    pages = Page.find_all_by_name('Name')
+    assert pages.kind_of?(Array)
+  end
+
+  def test_should_find_with_options_with_locale_set_to_es
+
+    I18n.locale = :es
+
+    pages = Page.find_all_by_name('Nombre')
+    assert_equal pages.length, 2
+
+    pages = Page.find_all_by_name('Nombre', :limit => 1)
+    assert_equal pages.length, 1
+
+    pages = Page.find_all_by_name('Nombre', :limit => 1, :order => "created_at DESC")
+    assert_equal pages.first.body, "Segundo Contenido"
+
+    pages = Page.find_all_by_name('Nombre', :limit => 1, :order => "created_at ASC")
+    assert_equal pages.first.body, "Contenido"
+
+  end
+
+  def test_wront_method_should_raise_nomethod_error
+    assert_raise NoMethodError do
+      Page.wadus_wadus
+    end
+  end
+
+end
