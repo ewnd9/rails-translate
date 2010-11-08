757b9e18a259a5c4afa122c649c8f67745c1b0ef
Francesc Esplugas <francesc@intraducibles.com>
2010-11-08 21:31:16 +0100

    Gardening

--- test/translate_test.rb
+++ test/translate_test.rb
@@ -12,21 +12,15 @@ def setup_db
   silence_stream(STDOUT) do
     ActiveRecord::Schema.define(:version => 1) do
       create_table :pages, :force => true do |t|
-        t.string :name_en, :default => "", :null => false
-        t.string :name_es, :default => "", :null => false
-        t.text :body_en, :default => "", :null => false
-        t.text :body_es, :default => "", :null => false
-        t.datetime :created_at
+        t.string :name_en
+        t.string :name_es
+        t.text :body_en
+        t.text :body_es
+        t.timestamps
       end
     end
   end
 
-end
-
-def teardown_db
-  ActiveRecord::Base.connection.tables.each do |table|
-    ActiveRecord::Base.connection.drop_table(table)
-  end
 end
 
 class Page < ActiveRecord::Base
@@ -57,35 +51,36 @@ class TranslateTest < Test::Unit::TestCase
   end
 
   def teardown
-    teardown_db
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.drop_table(table)
+    end
   end
 
   def test_should_get_title_en_of_the_page
-    page = Page.find(:first)
+    page = Page.first
     assert_equal page.send('name_en'), page.name
   end
 
   def test_should_get_title_es_of_the_page
     I18n.locale = :es
-    page = Page.find(:first)
+    page = Page.first
     assert_equal page.send('name_es'), page.name
   end
 
   def test_should_return_title_en_when_undefined_locale
     I18n.locale = :undefined
-    page = Page.find(:first)
+    page = Page.first
     assert_equal page.send('name_en'), page.name
   end
 
   def test_should_return_title_es_because_is_the_default_locale
     I18n.default_locale = :es
     I18n.locale = :undefined
-    page = Page.find(:first)
+    page = Page.first
     assert_equal page.send('name_es'), page.name
   end
 
   def test_should_find_by_translated_attribute
-
     page = Page.find_by_name('Name')
     assert_equal page.name, 'Name'
     assert_equal page.body, 'Content'
@@ -93,7 +88,6 @@ class TranslateTest < Test::Unit::TestCase
     I18n.locale = :es
     page = Page.find_by_name('Nombre')
     assert_equal page.name, 'Nombre'
-
   end
 
   def test_should_find_with_options_with_default_locale
@@ -102,7 +96,6 @@ class TranslateTest < Test::Unit::TestCase
   end
 
   def test_should_find_with_options_with_locale_set_to_es
-
     I18n.locale = :es
 
     pages = Page.find_all_by_name('Nombre')
@@ -116,10 +109,9 @@ class TranslateTest < Test::Unit::TestCase
 
     pages = Page.find_all_by_name('Nombre', :limit => 1, :order => "created_at ASC")
     assert_equal pages.first.body, "Contenido"
-
   end
 
-  def test_wront_method_should_raise_nomethod_error
+  def test_wrong_method_should_raise_nomethod_error
     assert_raise NoMethodError do
       Page.wadus_wadus
     end
